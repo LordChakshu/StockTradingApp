efc72d57203530660cc0a3bddd23aa9ada56c45a
LordChakshu <chakshukota74@gmail.com>
2023-08-01 22:47:45 +0530

    Routes Added

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const db = require("./db/db").mongoURI;
-const userProfileSchema =require('./models/user.js');
+const UserProfile=require('./models/user.js');
+
+const authRoutes=require('./routes/authRoutes');
 
 const app=express();
 
 // Connecting to MongoDB
 
 mongoose.connect(db, {useNewUrlParser: true}
-    ).then(() => console.log("MongoDB successfully connected!"))
-    .catch(err => console.log(err));
+    ).then(() => {
+        app.listen((3000));
+        console.log("MongoDB successfully connected!");}
+        ).catch(err => console.log(err));
 
 
-const port=3000;
-app.listen(port,()=>{
-    console.log("let's trade");
+app.get('/',(req,res)=>
+{
+   const user=new UserProfile({
+    firstName:"chakshu2",
+    lastName:"chatur",
+    email:"lord1@gmail.com",
+    password:"lord1234",
+    balance:10000
+   });
+   user.save().then((result)=>{
+    res.send(result);
+   }).catch((err)=>{
+    console.log(err);
+   });
 });
+
+app.use(authRoutes);
+
+
 
