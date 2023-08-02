18522544a507c64ed89f8e9f4f4c5064948ec8ef
LordChakshu <chakshukota74@gmail.com>
2023-08-02 23:47:12 +0530

    Authorization Controller Added

--- controller/authController.js
+++ controller/authController.js
@@ -1,9 +1,41 @@
+const mongoose=require('mongoose');
+const User=require('../models/user');
+const ValidRegisterInput=require('../validation/Register');
+const ValidLoginInput=require('../validation/login');
+
+
 module.exports.signup_post=(req,res)=>{
 
-    res.send('new signup');
+    const { error , isValid} =ValidRegisterInput(req.body);
+    
+    if(!isValid)
+    {
+      res.status(400).json(error);
+    }
+
+    User.findOne({email:req.body.email}).then(user=>{
+        
+        if(user){
+            res.status(400).json({email:'Email already Exists'});
+        }
+        else{
+            const newUser=User.create({
+                 email:req.body.email,
+                 password:req.body.password
+            });
+            res.status(201).json(newUser);
+            
+        }
+    })
+
 }
 
-module.exports.login_post=(req,res)=>{
+module.exports.login_post= async (req,res)=>{
 
-    res.send('new login');
+    const { error, isValid} =ValidLoginInput(req.body);
+
+    if(!isValid){
+     res.status(400).json(error);
+    }
+    
 }
