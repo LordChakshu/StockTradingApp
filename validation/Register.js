87951a4063de5628f5da93393e1f0c8c368d5b9e
LordChakshu <chakshukota74@gmail.com>
2023-08-03 17:19:20 +0530

    Register Validation Updated

--- validation/Register.js
+++ validation/Register.js
@@ -3,11 +3,21 @@ const isEmpty=require('is-empty');
 
 function validRegisterInput(data){
     
-    let errors={};
-    
+    let errors={ };
+    data.firstName=!isEmpty(data.firstName)?data.firstName:"";
+    data.lastName=!isEmpty(data.lastName)?data.lastName:"";
     data.email=!isEmpty(data.email)?data.email:"";
     data.password = !isEmpty(data.password) ? data.password : "";
     
+    //Name validation
+    if(Validator.isEmpty(data.firstName))
+    {
+        errors.firstName='Please Enter your First Name';
+    }
+    if(Validator.isEmpty(data.lastName))
+    {
+        errors.lastName='Please Enter your last Name';
+    }
     //email Validation
     if (Validator.isEmpty(data.email)) {
         errors.email = 'Email is required';
@@ -15,8 +25,7 @@ function validRegisterInput(data){
     else if (!Validator.isEmail(data.email)) {
         errors.email = 'Email is invalid';
     }
-
-      // password validation
+    // password validation
     if (Validator.isEmpty(data.password)) {
         errors.password = 'Password is required';
     }
