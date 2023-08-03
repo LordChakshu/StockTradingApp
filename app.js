bacaad26ecf4e483ef7793d0c67785054b150b74
LordChakshu <chakshukota74@gmail.com>
2023-08-03 17:13:04 +0530

    App.js Updated

--- app.js
+++ app.js
@@ -6,32 +6,17 @@ const UserProfile=require('./models/user.js');
 const authRoutes=require('./routes/authRoutes');
 
 const app=express();
+
 //Express JSON parser middleware
 app.use(express.json());
-// Connecting to MongoDB
 
+// Connecting to MongoDB
 mongoose.connect(db, {useNewUrlParser: true}
     ).then(() => {
         app.listen((3000));
         console.log("MongoDB successfully connected!");}
         ).catch(err => console.log(err));
 
-
-/*app.get('/',(req,res)=>
-{
-   const user=new UserProfile({
-    firstName:"chakshu2",
-    lastName:"chatur",
-    email:"lord1@gmail.com",
-    password:"lord1234",
-    balance:10000
-   });
-   user.save().then((result)=>{
-    res.send(result);
-   }).catch((err)=>{
-    console.log(err);
-   });
-});*/
 
 //Auth midleware
 app.use(authRoutes);
