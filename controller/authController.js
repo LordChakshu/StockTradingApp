eb54f18cf74eeff24209573c984442341361508d
LordChakshu <chakshukota74@gmail.com>
2023-08-03 17:14:39 +0530

    Signup controller Updated

--- controller/authController.js
+++ controller/authController.js
@@ -1,41 +1,48 @@
 const mongoose=require('mongoose');
 const User=require('../models/user');
+
 const ValidRegisterInput=require('../validation/Register');
 const ValidLoginInput=require('../validation/login');
 
+const bcrypt=require('bcrypt');
 
-module.exports.signup_post=(req,res)=>{
 
-    const { error , isValid} =ValidRegisterInput(req.body);
+module.exports.signup_post= async (req,res)=>{
+
+    const {errors,isValid}=ValidRegisterInput(req.body);
     
     if(!isValid)
     {
-      res.status(400).json(error);
+     console.log(errors);
+     return res.status(400).json(errors);
     }
 
-    User.findOne({email:req.body.email}).then(user=>{
+    const user= await User.findOne({email:req.body.email});
         
         if(user){
-            res.status(400).json({email:'Email already Exists'});
+            return res.status(400).json({email:'Email already Exists'});
         }
         else{
-            const newUser=User.create({
-                 email:req.body.email,
-                 password:req.body.password
-            });
-            res.status(201).json(newUser);
-            
-        }
-    })
+            const salt= await bcrypt.genSalt();
+            const hashedPassword= await bcrypt.hash(req.body.password,salt);
 
-}
+            const newUser=await User.create({
+                 firstName:req.body.firstName,
+                 lastName:req.body.lastName,
+                 email:req.body.email,
+                 password:hashedPassword
+            });
+            return res.status(201).json(newUser);
+        }
+    }
+
 
 module.exports.login_post= async (req,res)=>{
 
     const { error, isValid} =ValidLoginInput(req.body);
 
     if(!isValid){
-     res.status(400).json(error);
+     return res.status(400).json(error);
     }
     
 }
