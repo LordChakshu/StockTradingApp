62a55eb1301f0b0a34d52186e4c65fc2945734e2
LordChakshu <chakshukota74@gmail.com>
2023-08-11 16:15:50 +0530

    app.js Updated

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require('express');
 const mongoose=require('mongoose');
 const db = require("./db/db").mongoURI;
 const UserProfile=require('./models/user.js');
+const cors=require('cors');
 
 const authRoutes=require('./routes/authRoutes');
 
@@ -9,6 +10,9 @@ const app=express();
 
 //Express JSON parser middleware
 app.use(express.json());
+
+//cors middleware to allow all origins
+app.use(cors());
 
 // Connecting to MongoDB
 mongoose.connect(db, {useNewUrlParser: true}
